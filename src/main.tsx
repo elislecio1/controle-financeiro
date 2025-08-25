ae9676fcca30996e6f66b4b40b15b932c13bf32f
elislecio1 <elislecio@gmail.com>
2025-08-25 17:42:19 -0300

    Corrigir rota de pricing e schema simplificado

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App'
 import AuthCallback from './pages/AuthCallback'
 import UserManagement from './pages/UserManagement'
+import Pricing from './pages/Pricing'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
@@ -12,6 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <Routes>
         <Route path="/auth/callback" element={<AuthCallback />} />
         <Route path="/admin/users" element={<UserManagement />} />
+        <Route path="/pricing" element={<Pricing />} />
         <Route path="/*" element={<App />} />
       </Routes>
     </BrowserRouter>
